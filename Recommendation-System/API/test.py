04b19de1443cd006f838dfce300aa7088601af0a
ogup0002 <112609333+ogup0002@users.noreply.github.com>
2022-09-14 16:38:16 +1000

    Heroku config

--- Recommendation-System/API/test.py
+++ Recommendation-System/API/test.py
@@ -1,15 +1,27 @@
 import requests
 
-BASE = "http://127.0.0.1:5000/"
+# BASE = "http://127.0.0.1:5000/"
+BASE = "https://sittofit-backend-ogup0002.vercel.app/trial"
+ 
+# response = requests.put(BASE + "rating", {"web_id": 12001, "iid": 1002,"rating": 1})
 
-response = requests.put(BASE + "rating", {"web_id": 12001, "iid": 1002,"rating": 1})
-
-print(response.json())
+# print(response.json())
 
 # input()
 
-# response = requests.get(BASE + 'gett')
+# response = requests.get(BASE + 'cards')
 # print(response.json())
 
 # response = requests.get(BASE + "preference/1")
+# print(response.json())
+
+response = requests.get(BASE)
+print(response.json())
+
+# import requests 
+# import json
+
+# BASE = "http://1be2-121-200-5-152.ngrok.io" 
+
+# response = requests.put(BASE, {'link' : 'C:\\Users\\pragya\\Downloads\\Iteration 2\\test\\new_holland_honeyeater_test_1.wav'})
 # print(response.json())
